39f96ffce276bc2551b711793b03d49c11f6966c
Ubuntu <ubuntu@ip-172-31-3-225.us-west-2.compute.internal>
2015-07-05 14:37:20 +0000

    adding base yii application with minor branding updates

---
+++ app/views/site/index.php
@@ -0,0 +1,39 @@
+<?php
+/* @var $this yii\web\View */
+$this->title = 'My Yii Application';
+?>
+<div class="site-index">
+
+    <div class="jumbotron">
+        <h1>Welcome to Tiff Miff!</h1>
+
+        <p class="lead">Resolving complex debates by finger tapping and swiping....</p>
+
+        <p><a class="btn btn-lg btn-success" href="http://tiffmiff.com/index.php?r=site%2Flogin">Get Tiffing! .. or Miffing</a></p>
+    </div>
+
+    <div class="body-content">
+
+        <div class="row">
+            <div class="col-lg-4">
+                <h2>Tiff</h2>
+		<p>When you tiff a topic you enter into the topic's world that has users, opining, sharing, 
+			recording, watching and waiting for results</p>
+                <p><a class="btn btn-default" href="http://tiffmiff.com/index.php?r=site%2Flogin">Tiff now!</a></p>
+            </div>
+            <div class="col-lg-4">
+                <h2>Miff</h2>
+		<p>When you miff a topic you simply move on to the next possible tiffable topic. Tiff miff learns your
+			habbits so we dont show you topics you are not interested in,</p>
+                <p><a class="btn btn-default" href="http://tiffmiff.com/index.php?r=site%2Flogin">Miff now!</a></p>
+            </div>
+            <div class="col-lg-4">
+                <h2>Uff</h2>
+		<p>Find tiffing miffing too much? Just double tap and read the results, analysis, 
+			just browse photos, and what not, find like minded people</p>
+                <p><a class="btn btn-default" href="http://tiffmiff.com/index.php?r=site%2Flogin">Uff now!</a></p>
+            </div>
+        </div>
+
+    </div>
+</div>
